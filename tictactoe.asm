3315b33b8136bc8c8c89325e6f034171662f7fb4
zirconion <trulyzirconion@gmail.com>
2022-04-02 21:32:39 -0500

    Update tictactoe.asm

--- tictactoe.asm
+++ tictactoe.asm
@@ -1,1 +1,5 @@
-#whee
+.data
+win3s: .word 73, 7, 273, 146, 56, 448, 84, 292 #winning3s (147(73),123(7),159(273),258(146),456(56),789(448),357(84),369(292))
+position: .word 0x10010020	#base address for array position, position[1] = unmarked,  position[2] = x's, position[3] = o's
+
+.text
