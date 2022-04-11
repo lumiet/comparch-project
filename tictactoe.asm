a0e69bd43796aa795aa79bd2956d3a88fe86f2b8
zirconion <102887343+zirconion@users.noreply.github.com>
2022-04-11 15:27:57 -0500

    Update tictactoe.asm

--- tictactoe.asm
+++ tictactoe.asm
@@ -1,24 +1,72 @@
 .data
 win3s: .word 73, 7, 273, 146, 56, 448, 84, 292 #winning3s (147(73),123(7),159(273),258(146),456(56),789(448),357(84),369(292))
-positionkey: .word 1, 2, 4, 8, 16, 32, 64, 128, 256 #array for integer to board key array, i. e. positionkey[1] = 000000001, positionkey[4] = 000001000
-# corresponds integer to board position
+positionKey: .word 1, 2, 4, 8, 16, 32, 64, 128, 256 #array for integer to board key array, i. e. positionkey[0] = 000000001, positionkey[3] = 000001000
+# corresponds integer to board position (int n = board position bit string 2^(n-1))
 # key: 7 8 9
 #      4 5 6
 #      1 2 3
-position: .word 511, 0, 0	#base address for array position, position[1] = unmarked,  position[2] = x's, position[3] = o's
-
+position: .word 511, 0, 0	#base address for array position, position[0] = unmarked,  position[1] = x's, position[2] = o's
+							#board starts fully unmarked
+playerPrompt: .asciiz "Player move: "
+newLine: .asciiz "\n"
+AIMovePref: .word 16, 64, 256, 1, 4, 128, 8, 32, 2 # (5 > 7 > 9 > 1 > 3 > 8 > 4 > 6 > 2) (center > corners > sides)
 
 
 .text
 
+	#load position address
+	la $s7, position
+	
+gameLoop:
+	#display position
+	li $v0, 1 #print integer
+	lw $a0, 0($s7) #print unmarked
+	syscall
+	li $v0, 4 # print string
+	la $a0, newLine #load new line
+	syscall
+	li $v0, 1
+	lw $a0, 4($s7) #print print x's
+	syscall
+	li $v0, 4 # print string
+	la $a0, newLine #load new line
+	syscall
+	li $v0, 1
+	lw $a0, 8($s7) #print o's
+	syscall
 
-	#andCount test
-	li $a0, 420
-	li $a1, 235
-	jal andCount 
+	#player move
+	li $v0, 4 # print string
+	la $a0, playerPrompt #load player prompt
+	syscall
+
+	li $v0, 5 # read integer, will make move for player
+	syscall
+
+	# convert number to position
+	li $s0, 1 # load 1 into s0
+	subi $v0, $v0, 1
+	sllv $s0, $s0, $v0 # convert input number into board position, 2^(n - 1)
+
+	#check if position is filled
+	lw $t0, 0($s7) # 4($s7) is position[1], which is the unmarked bit string
+	and $t1, $s0, $t0 # binary and unmarked and desired move, store result in $t1
+	beq $t1, 0, gameLoop # !restarts loop if desired move is unavailable, maybe add message to check if valid/seperate error handling
+
+	#play move
+	sub $t0, $t0, $s0 # subtract desired move from unmarked
+	sw $t0, 0($s7) # store changed unmarked position
+	lw $t0, 4($s7) # load x/player position
+	add $t0, $t0, $s0 # add desired move to player position
+	sw $t0, 4($s7) # store player position
+	
+	#computer move
 	
 	
-	j Exit
+	j gameLoop #temp end of gameloop
+	
+	
+	j Exit #exit
 
 andCount: # takes bit string (of length 9) in a0 and a1, returns number of 1s they have in common
 	subu $sp, $sp, 32 #allocate stack
